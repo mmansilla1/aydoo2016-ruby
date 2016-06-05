74793a42a5b35992fed8c5e4a5d8db35deaef706
Matias Mansilla <tute.mdm@gmail.com>
2016-06-05 19:42:35 -0300

    Se sube identificador de opciones

---
+++ factores-primos/model/identificador_de_opciones.rb
@@ -0,0 +1,44 @@
+class IdentificadorDeOpciones
+
+	@argumentos = []
+	@numero_a_factorizar = 0
+	@format = ""
+	@sort = "";
+	@output = ""
+	
+  def initialize(argumentos)     
+    @argumentos = argumentos    
+  end
+
+  def identificar_opciones()
+    @numero_a_factorizar = @argumentos[0]
+    for indice in 1...@argumentos.length do
+      parametro_a_evaluar = @argumentos[indice][0, 3] 
+      case parametro_a_evaluar
+	when "--F"
+	  @format = @argumentos[indice]
+	when "--S"
+	  @sort = @argumentos[indice]
+	when "--O"
+	  @output = @argumentos[indice]
+      end
+    end
+  end
+	
+  def numero_a_factorizar
+    @numero_a_factorizar
+  end
+
+  def format
+    @format
+  end
+
+  def sort
+    @sort
+  end
+
+  def output
+    @output
+  end  	
+
+end
---
+++ factores-primos/spec/identificador_de_opciones_spec.rb
@@ -0,0 +1,24 @@
+require 'rspec' 
+require_relative '../model/IdentificadorDeOpciones'
+
+describe 'IdentificadorDeOpciones' do
+
+  it 'deberia retornar 360 cuando numero_a_factorizar' do
+    argumentos = [360]
+    identificador_de_opciones = IdentificadorDeOpciones.new(argumentos)
+
+    identificador_de_opciones.identificar_opciones()
+    
+    expect(identificador_de_opciones.numero_a_factorizar).to eq 360  
+  end
+
+  it 'deberia retornar "--FORMAT=PRETTY" cuando format' do
+    argumentos = [0, "--FORMAT=PRETTY"]
+    identificador_de_opciones = IdentificadorDeOpciones.new(argumentos)
+
+    identificador_de_opciones.identificar_opciones()
+    
+    expect(identificador_de_opciones.format).to eq "--FORMAT=PRETTY"
+  end
+
+end
