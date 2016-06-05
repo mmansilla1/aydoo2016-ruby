003d9250e7247caf0d2efee2f8f7824a7cd7ee66
Matias Mansilla <tute.mdm@gmail.com>
2016-06-05 20:01:47 -0300

    Arreglo de errores en nombres de archivos

---
+++ factores-primos/spec/IdentificadorDeOpciones_spec.rb
@@ -0,0 +1,42 @@
+require 'rspec' 
+require_relative '../model/IdentificadorDeOpciones'
+
+describe 'IdentificadorDeOpciones' do
+
+  it 'deberia retornar 360 cuando numero_a_factorizar' do
+    argumentos = [360]
+    identificador_de_opciones = IdentificadorDeOpciones.new(argumentos)
+
+    identificador_de_opciones.identificar_opciones()
+    
+    expect(identificador_de_opciones.numero_a_factorizar).to eq 360  
+   end
+
+  it 'deberia retornar "--FORMAT=PRETTY" cuando format' do
+    argumentos = [0, "--FORMAT=PRETTY"]
+    identificador_de_opciones = IdentificadorDeOpciones.new(argumentos)
+
+    identificador_de_opciones.identificar_opciones()
+    
+    expect(identificador_de_opciones.format).to eq "--FORMAT=PRETTY"
+   end
+
+  it 'deberia retornar "--SORT:ASC" cuando sort' do
+    argumentos = [0, "--FORMAT=PRETTY", "--SORT:ASC"]
+    identificador_de_opciones = IdentificadorDeOpciones.new(argumentos)
+
+    identificador_de_opciones.identificar_opciones()
+    
+    expect(identificador_de_opciones.sort).to eq "--SORT:ASC"
+   end
+
+  it 'deberia retornar "--OUTPUT-FILE:ARCHIVOSALIDA.TXT" cuando output' do
+    argumentos = [0, "--FORMAT=PRETTY", "--SORT:ASC", "--OUTPUT-FILE:ARCHIVOSALIDA.TXT"]
+    identificador_de_opciones = IdentificadorDeOpciones.new(argumentos)
+
+    identificador_de_opciones.identificar_opciones()
+    
+    expect(identificador_de_opciones.output).to eq "--OUTPUT-FILE:ARCHIVOSALIDA.TXT"
+   end
+
+end
