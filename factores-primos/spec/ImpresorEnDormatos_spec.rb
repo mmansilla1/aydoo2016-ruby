a6ef003c3e2f974b93b1859d517912e021f8deda
Matías Mansilla <tute.mdm@gmail.com>
2016-06-07 00:05:20 -0300

    Nuevo test para ImpresorEnFormatos

--- factores-primos/spec/ImpresorEnDormatos_spec.rb
+++ factores-primos/spec/ImpresorEnDormatos_spec.rb
@@ -14,5 +14,17 @@ describe 'ImpresorEnFormatos' do
     
     expect(impresion_esperada).to eq impresion_obtenida
   end
+  
+  it 'deberia imprimir "2\n2\n2\n3\n3\n5\n" " cuando get_impresion_resultante' do
+    
+    numero_a_factorizar = 360
+    impresor_en_formatos = ImpresorEnFormatos.new()
+    lista_de_factores_primos = [2, 2, 2, 3, 3, 5]
+    impresion_esperada = "2\n2\n2\n3\n3\n5\n";
+
+    impresion_obtenida = impresor_en_formatos.get_impresion_resultante("--format=quiet", numero_a_factorizar, lista_de_factores_primos, "")
+    
+    expect(impresion_esperada).to eq impresion_obtenida
+  end
 
 end
