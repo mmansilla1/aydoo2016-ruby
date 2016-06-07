e78205e263818c983b81c2a155ab5001b514f375
Matías Mansilla <tute.mdm@gmail.com>
2016-06-07 00:33:46 -0300

    Se suben las clases Persistidor y su respectivo test

---
+++ factores-primos/spec/Persistidor_spec.rb
@@ -0,0 +1,18 @@
+require 'rspec' 
+require_relative '../model/Persistidor'
+
+describe 'Persistidor' do
+
+  it 'deberia persistir en archivo en el directorio del proyecto cuando escribir_factorizacion' do
+    
+    impresion_esperada = "Factores primos 90: 2 2 2 3 3 5 \n"
+    path = "salida.txt"
+    persistidor = Persistidor.new(path)
+    persistidor.escribir_factorizacion(impresion_esperada)
+    
+    impresion_obtenida = persistidor.leer_factorizacion
+    
+    expect(impresion_esperada).to eq impresion_obtenida
+  end
+
+end
