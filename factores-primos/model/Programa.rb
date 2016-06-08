4fc09985a1c06d12c80b70f34a85dd9166d0157a
Matías Mansilla <tute.mdm@gmail.com>
2016-06-07 23:09:11 -0300

    Modificacion en la clase Programa

--- factores-primos/model/Programa.rb
+++ factores-primos/model/Programa.rb
@@ -1,58 +1,68 @@
+require_relative '../model/IdentificadorDeOpciones'
+require_relative '../model/DescomponedorEnFactores'
+require_relative '../model/ImpresorEnFormatos'
+require_relative '../model/PersistidorEnArchivo'
+
 class Programa
 
-	@numero_a_factorizar = 0
-	@format = ""
-	@sort = ""
-	@path = ""
-	
-	def main(argumentos)
-		
-		get_parametros_aplicacion(argumentos)
-		
-		if numero_a_factorizar != 0
-			
-			lista_de_factores_primos = get_lista_de_factores_primos(numero_a_factorizar);
-	    	impresionResultante = get_impresion_resultante(@format, @numero_a_factorizar, lista_de_factores_primos, @sort)
-	    	
-	    	if @pathImpresion == ""
-	    		puts impresion_resultante
-	    	else
-	    		persistir_en_archivo(@path, impresion_resultante)
-	    	end	
-		else
-			puts "Debe indicar un numero."
-		end
-	end
-	
-	def get_lista_de_factores_primos
-    	
-		descomponedor_en_factores = DescomponedorEnFactores.new
-    	lista_de_factores_primos = descomponedor_en_factores.descomponer_en_factores_primos(@numero_a_factorizar)
-    	return lista_de_factores_primos
-	end
-	
-	def get_parametros_aplicacion(argumentos)
-	
-		identificador_de_opciones = IdentificadorDeOpciones.new(argumentos)
-		@numero_a_factorizar = identificador_de_opciones.get_numero_a_factorizar		
-		@format = identificador_de_opciones.get_format
-		@sort = identificador_de_opciones.get_sort
-		@path = identificador_de_opciones.get_output
-	end
-	
-	def get_impresion_resultante(lista_de_factores_primos) 
+    @numero_a_factorizar
+    @format
+    @sort
+    @path
 
-		impresor_en_formatos = ImpresorEnFormatos.new
-		impresion_resultante = impresor_en_formatos.get_impresion_resultante(@format, @numero_a_factorizar, lista_de_factores_primos, @sort)
-		return impresion_resultante
-	end	
-	
-	def persistir(impresion_a_persistir) 
+    def main(argumentos)
+        
+        sin_output = "sinOutput"
+        get_parametros_aplicacion(argumentos)
 
-		posicion_path_real = 14;
-		path_real = @path[0, posicion_path_real]
-		persistidor = Persistidor.new(path_real)
-		persistidor.escribir_factorizacion(impresion_a_persistir)		
-	end	
+        if @numero_a_factorizar != 0
+
+            lista_de_factores_primos = get_lista_de_factores_primos
+            impresion_resultante = get_impresion_resultante(lista_de_factores_primos)
+            if @path == sin_output
+                puts impresion_resultante
+            else
+                persistir(impresion_resultante)
+            end
+        else
+            puts "Debe indicar un numero."
+        end
+    end
+
+    private
+    def get_parametros_aplicacion(argumentos)
+        
+        identificador_de_opciones = IdentificadorDeOpciones.new(argumentos)
+        identificador_de_opciones.identificar_opciones()
+        @numero_a_factorizar = identificador_de_opciones.numero_a_factorizar        
+        @format = identificador_de_opciones.format
+        @sort = identificador_de_opciones.sort
+        @path = identificador_de_opciones.output
+    end
+
+    private
+    def get_lista_de_factores_primos
+        
+        descomponedor_en_factores = DescomponedorEnFactores.new
+        lista_de_factores_primos = descomponedor_en_factores.descomponer_en_factores_primos(@numero_a_factorizar.to_i)
+        return lista_de_factores_primos
+    end
+
+    private
+    def get_impresion_resultante(lista_de_factores_primos) 
+
+        impresor_en_formatos = ImpresorEnFormatos.new
+        impresion_resultante = impresor_en_formatos.get_impresion_resultante(@format, @numero_a_factorizar.to_i, lista_de_factores_primos, @sort)
+        return impresion_resultante
+    end 
+
+    private
+    def persistir(impresion_a_persistir) 
+
+        posicion_path_real = 14;
+        path_real = @path[posicion_path_real, @path.length]
+        persistidor = Persistidor.new(path_real)
+        persistidor.escribir_factorizacion(impresion_a_persistir)
+    end 
 
 end
